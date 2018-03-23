40a65a8c2b27574cd226cc19a54ea79b9a190c47
Anne Gabrielle <annegabs65@gmail.com>
2018-03-23 17:40:54 -0300

    adicionando tela inicial

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-      <text>OLÁ</text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#16ad37',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import App from './app/index';
+ 
+export default App;
---
+++ app/index.js
@@ -0,0 +1,6 @@
+EStyleSheet.build({
+    $primaryBlue: '#4F6D7A',
+    $white: '#FFFFFF'
+});
+ 
+export default () => <Home />;
